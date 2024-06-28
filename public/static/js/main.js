a4efacae8e5fad0451cd75b81b6e08db91e0c68b
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-06-28 01:02:33 -0500

    console js error

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -12,7 +12,7 @@
    * Prevent multiple form submissions
    */
   document.addEventListener('DOMContentLoaded', function () {
-    var form = document.querySelector('.newsletter-subscribe-form');
+    var form = document.querySelector('.contact-form');
     var submitButton = document.getElementById('contact-submit-button');
   
     form.addEventListener('submit', function () {
